ebf25d67c6e83df35f88ad0b284443c17b03fc86
Talha <tkz9696@gmail.com>
2022-08-01 19:06:26 +0500

    Fix spacing errors

--- Person.rb
+++ Person.rb
@@ -1,18 +1,18 @@
 class Person
-    attr_accessor :name, :age
-    attr_reader :id
-    
-    def initialize(name = "Unknown", age, parent_permission = true)
-        @id = rand(10)
-        @name = name
-        @age = age
-    end
-
-    private def is_of_age?
-        @age >= 18
-    end
-
-    def can_use_services?
-        is_of_age? || parent_permission
-    end
+  attr_accessor :name, :age
+  attr_reader :id
+  
+  def initialize(name = "Unknown", age, parent_permission = true)
+    @id = rand(10)
+    @name = name
+    @age = age
+  end
+  
+  private def is_of_age?
+    @age >= 18
+  end
+  
+  def can_use_services?
+    is_of_age? || parent_permission
+  end
 end
--- Teacher.rb
+++ Teacher.rb
@@ -1,10 +1,10 @@
 class Teacher < Person
-    def initialize(specialization)
-        super
-        @specialization = specialization
-    end
-
-    def can_use_services?
-        true
-    end
+  def initialize(specialization)
+    super
+    @specialization = specialization
+  end
+  
+  def can_use_services?
+    true
+  end
 end
