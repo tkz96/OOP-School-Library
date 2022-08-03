7ff16101e6f4d59f018133214fdb378b8da40e8f
Talha <tkz9696@gmail.com>
2022-08-03 19:35:34 +0500

    Add classes and decorators

--- school_person.rb
+++ school_person.rb
@@ -1,10 +1,10 @@
 class Nameable
   def correct_name
-    raise NotImplementedError
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
   end
 end
 
-class Person
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -19,9 +19,45 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+class Decorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.upcase
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name[0, 10]
+  end
+end
+
+# Try the following code and check if you managed to decorate your person:
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+capitalizedTrimmedPerson.correct_name
