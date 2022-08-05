260aa97577674238004baca2c5471325d506bcd7
Talha <tkz9696@gmail.com>
2022-08-05 15:58:20 +0500

    Remove deprecated student setter

--- school_book.rb
+++ school_book.rb
@@ -1,14 +1,14 @@
 class Book
-    attr_accessor :rentals
-    attr_reader :title, :author
+  attr_accessor :rentals
+  attr_reader :title, :author
 
-    def initialize(title, author)
-        @title = title
-        @author = author
-        @rentals = []
-    end
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
 
-    def add_rental(rental)
-        @rentals.push(rental)
-    end
+  def add_rental(rental)
+    @rentals.push(rental)
+  end
 end
--- school_student.rb
+++ school_student.rb
@@ -4,10 +4,7 @@ class Student < Person
   def initialize(classroom)
     super
     @classroom = classroom
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
