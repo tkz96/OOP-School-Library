5300d3fad4d853dcccaf2c80e974a2979c0459cd
Talha <tkz9696@gmail.com>
2022-08-16 15:52:13 +0500

    Refactored main.rb and app.rb file

--- app.rb
+++ app.rb
@@ -3,46 +3,39 @@ require './school_person'
 require './school_student'
 require './school_teacher'
 require './school_book'
-require './main'
 
 class App
-  def initialize
-    @books = []
-    @rentals = []
-    @people = []
-  end
-
   # 1
-  def list_books
-    if @books.empty?
+  def list_books(books)
+    if books.empty?
       puts 'Error: No books added!'
     else
-      @books.each_with_index do |book, index|
+      books.each_with_index do |book, index|
         puts "#{index}) Title: #{book.title}, Author: #{book.author}"
       end
     end
   end
 
   # 2
-  def list_people
-    if @people.empty?
+  def list_people(people)
+    if people.empty?
       puts 'Error: No people added'
     else
-      @people.each_with_index do |person, index|
+      people.each_with_index do |person, index|
         puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
     end
   end
 
   # 3
-  def create_person
+  def create_person(people)
     print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     person_type = gets.chomp
     case person_type
     when '1'
-      create_student
+      create_student(people)
     when '2'
-      create_teacher
+      create_teacher(people)
     else
       puts 'error'
     end
@@ -50,7 +43,7 @@ class App
   end
 
   # 3 (1)
-  def create_student
+  def create_student(people)
     print 'Age: '
     age = gets.chomp
 
@@ -70,11 +63,11 @@ class App
     end
 
     student = Student.new(nil, age, name, permission)
-    @people.push(student)
+    people.push(student)
   end
 
   # 3 (2)
-  def create_teacher
+  def create_teacher(people)
     print 'Age: '
     age = gets.chomp
 
@@ -85,11 +78,11 @@ class App
     specialization = gets.chomp
 
     teacher = Teacher.new(age, name, specialization)
-    @people.push(teacher)
+    people.push(teacher)
   end
 
   # 4
-  def create_book
+  def create_book(books)
     print 'Title: '
     title = gets.chomp
 
@@ -97,41 +90,33 @@ class App
     author = gets.chomp
 
     new_book = Book.new(title, author)
-    @books.push(new_book)
+    books.push(new_book)
     puts 'Book added successfully'
-
-    Main.new.run
   end
 
   # 5
-  def create_rental
+  def create_rental(books, people, rentals)
     puts 'Select a book from the following list by number: '
     # list_books
-    @books.map.with_index do |book, index|
-      puts "#{index}) Title: #{book.title}, Author: #{book.author}"
-    end
+    list_books(books)
     book_idx = gets.chomp
     puts 'Select a person from the following list by number: '
     # list_people
-    @people.map.with_index do |person, index|
-      puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
+    list_people(people)
     person_idx = gets.chomp
 
     print 'Date(YYYY/MM/DD): '
     rental_date = gets.chomp
 
-    rental = Rental.new(rental_date, @people[person_idx.to_i], @books[book_idx.to_i])
-    @rentals.push(rental)
+    rental = Rental.new(rental_date, people[person_idx.to_i], books[book_idx.to_i])
+    rentals.push(rental)
     puts 'Rental added successfully!'
-
-    Main.new.run
   end
 
-  def list_rental
+  def list_rental(rentals)
     print "\nID of person: "
     person_id = gets.chomp
-    @rentals.each do |rental|
+    rentals.each do |rental|
       next unless rental.person.id == person_id.to_i
 
       puts "Title: #{rental.book.title}"
--- main.rb
+++ main.rb
@@ -1,40 +1,42 @@
 require './app'
 
-class Menu
+def main
+  books = []
+  people = []
+  rentals = []
+  run(books, people, rentals)
+end
+
+def show_menu
+  puts "Welcome to School Library App!\n\n"
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts "7 - Exit\n\n"
+end
+
+def run(books, people, rentals)
   app = App.new
   loop do
-    show_menu
-    input(app)
-  end
-
-  def show_menu
-    puts "Welcome to School Library App!\n\n"
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts "7 - Exit\n\n"
-  end
-
-  def input(app)
     show_menu
     option = gets.chomp
     case option
     when '1'
-      app.list_books
+      app.list_books(books)
     when '2'
-      app.list_people
+      app.list_people(people)
     when '3'
-      app.create_person
+      app.create_person(people)
     when '4'
-      app.create_book
+      app.create_book(books)
     when '5'
-      app.create_rental
+      app.create_rental(books, people, rentals)
     when '6'
-      app.list_rental
+      app.list_rental(rentals)
     else
       puts 'Good-bye.'
       exit
@@ -42,10 +44,4 @@ class Menu
   end
 end
 
-def main
-  app = App.new
-
-  run
-
-  show_menu app
-end
+main
